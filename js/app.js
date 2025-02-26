d2b3bd1c000780c600507f74ae1a29a33720e80d
megaifiok_001 <ifiokaniebiet@gmail.com>
2025-02-26 12:06:34 +0100

    only one

--- js/app.js
+++ js/app.js
@@ -387,6 +387,19 @@ $(function() {
     .append('<div class="picture"></div>')
     .children('.picture').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
   });
+
+
+  document.querySelectorAll('.gallery__link').forEach(function(element) {
+    element.addEventListener('click', function(event) {
+      event.preventDefault();
+      const url = element.getAttribute('data-url');
+      if (url) {
+        window.location.href = url;
+      } else {
+        console.error('URL not found in data-url attribute.');
+      }
+    });
+  });
   // --------------------------------------------- //
   // PhotoSwipe Gallery Images Replace End
   // --------------------------------------------- //
